fb708d31d2796aebcc84f3aa6fb75b1197b035c2
Suganya Thurairatnam <kichaa4@gmail.com>
2015-07-26 13:14:45 +0530

    update

--- ex_nonlinearfilter.m
+++ ex_nonlinearfilter.m
@@ -3,9 +3,17 @@ if (length(size(I))==3)
     I=rgb2gray(I);
 end
 
-subplot(2,2,1);imshow(I);
-subplot(2,2,2);h=histogram(I);plot(h);
+subplot(2,4,1);imshow(I);
+subplot(2,4,5);h=histogram(I);plot(h);
 
-J=medianfilter(I);
-subplot(2,2,3);imshow(J);
-subplot(2,2,4);h=histogram(J);plot(h);
+J=minfilter(I);
+subplot(2,4,2);imshow(J);
+subplot(2,4,6);h=histogram(J);plot(h);
+
+K=maxfilter(I);
+subplot(2,4,3);imshow(K);
+subplot(2,4,7);h=histogram(K);plot(h);
+
+L=medianfilter(I);
+subplot(2,4,4);imshow(L);
+subplot(2,4,8);h=histogram(L);plot(h);
